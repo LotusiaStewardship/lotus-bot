50723c5f7333df8bdfc017ed7f35164ba03de9c4
maff1989 <matthew@urgero.net>
2025-04-11 16:52:05 -0700

    Add Temporal client, `LocalActivities`
    The Temporal client will be critical for workflow automation. `LocalActivities`
    are defined to be used in workflow execution

--- lib/temporal/index.ts
+++ lib/temporal/index.ts
@@ -1,8 +1,3 @@
 import LotusBot from '../lotusbot'
-export type LotusBotActivities = typeof LotusBot.prototype.temporal
-/** Export defined Temporal methods from LotusBot prototype as the Worker activities */
-/*
-export interface WorkerActivities {
-  sendMessage: LotusBotActivities['sendMessage']
-}
-*/
+export type Activities = typeof LotusBot.prototype.temporalActivities
+export type LocalActivities = typeof LotusBot.prototype.temporalLocalActivities
